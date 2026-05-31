75a66e0c50f2ded89887d109758626fbc6ef78c9
tux2won <114200466+tux2won@users.noreply.github.com>
2026-05-31 21:32:09 +0900

    [level 5] Title: 멸종위기의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
+++ 프로그래머스/5/301651. 멸종위기의 대장균 찾기/멸종위기의 대장균 찾기.sql
@@ -1,30 +1,41 @@
--- 최초 세대의 대장균들에게 '세대(=DEPTH)'를 1로 설정
-WITH RECURSIVE GEN
-AS (
-    SELECT ID, PARENT_ID, 1 AS DEPTH 
-    FROM ECOLI_DATA
-    WHERE PARENT_ID IS NULL -- 부모가 없으니 1세대
+-- WITH RECURSIVE: CTE 안에서 자기 자신을 참조 가능함
+
+WITH RECURSIVE generation_info AS (
+    SELECT
+        ecoli.id AS id,
+        ecoli.parent_id AS parent_id,
+        1 AS generation
+    FROM ecoli_data AS ecoli
+    WHERE 1=1
+        AND ecoli.parent_id IS NULL
 
     UNION ALL
 
-    SELECT CHILD.ID, CHILD.PARENT_ID, GEN.DEPTH + 1 -- 그 다음 세대(CHILD라고 지정함)부터는 +1 하나씩 해줌
-    FROM ECOLI_DATA CHILD
-    JOIN GEN ON CHILD.PARENT_ID = GEN.ID -- 부모 ID가 윗 세대 ID와 동일하면 세대(DEPTH)정보 get해와서 위 SELECT문처럼 +1해주면 그 놈은 그 다음 세대가 되.
+    SELECT
+        child_ecoli.id AS id,
+        child_ecoli.parent_id AS parent_id,
+        parent_generation.generation + 1 AS generation
+    FROM ecoli_data AS child_ecoli
+        JOIN generation_info AS parent_generation
+            ON child_ecoli.parent_id = parent_generation.id
 ),
-
-CHILD
-AS (
-    SELECT A.ID, COUNT(B.ID) NUM 
-    FROM ECOLI_DATA A
-    LEFT JOIN ECOLI_DATA B
-    ON A.ID=B.PARENT_ID -- A가 부모, B가 자식
-    GROUP BY A.ID
+child_count AS (
+    SELECT
+        parent_ecoli.id AS id,
+        COUNT(child_ecoli.id) AS child_count
+    FROM ecoli_data AS parent_ecoli
+        LEFT JOIN ecoli_data AS child_ecoli
+            ON parent_ecoli.id = child_ecoli.parent_id
+    GROUP BY parent_ecoli.id
 )
 
--- 최종 결과를 구하는 쿼리
-SELECT COUNT(*) AS COUNT, A.DEPTH AS GENERATION
-FROM GEN A
-JOIN CHILD B ON A.ID = B.ID -- GEN과 CHILD를 ID로 조인
-WHERE B.NUM = 0 -- 자식이 없는 개체만 필터링
-GROUP BY A.DEPTH -- 세대별로 그룹화
-ORDER BY A.DEPTH; -- 세대 순으로 정렬
+SELECT
+    COUNT(*) AS count,
+    generation_info.generation AS generation
+FROM generation_info AS generation_info
+    JOIN child_count AS child_count
+        ON generation_info.id = child_count.id
+WHERE 1=1
+    AND child_count.child_count = 0
+GROUP BY generation
+ORDER BY generation;
